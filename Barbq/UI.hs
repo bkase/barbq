2b73f92428885da063e2fecc0992c18f818d2b20
Brandon Kase <bkase@o1labs.org>
2019-11-16 22:51:12 -0500

    UI finally works!

--- Barbq/UI.hs
+++ Barbq/UI.hs
@@ -37,39 +37,19 @@ pairDay p1 p2 f (Day f1 g1 g) (Day f2 g2 h) =
   let (y1, y2) = p2 (,) g1 g2 in
   f (g x1 y1) (h x2 y2)
 
-type Responder = Event -> RenderM ()
+type SendResult s = Endo s
+type Responder s = Event -> SendResult s
 
-newtype RenderM a = RenderM (StateT ([RenderM ()], Responder) (ReaderT Vty IO) a)
-  deriving (Functor, Applicative, Monad, MonadReader Vty, MonadState ([RenderM ()], Responder), MonadIO)
+newtype RenderM a = RenderM (ReaderT Vty IO a)
+  deriving (Functor, Applicative, Monad, MonadReader Vty, MonadIO)
 
 runRenderM :: RenderM () -> Vty -> IO ()
-runRenderM rm vty = do
-  next <- redraw rm vty
-  continue next
-  where
-    redraw :: RenderM () -> Vty -> IO Responder
-    redraw (RenderM st) vty  = do
-      (a, (actions, next)) <- runReaderT (runStateT st ([], const $ return ())) vty
-      nexts <- forM actions (`redraw` vty)
-      return $ foldr (\next1 build e -> next1 e >>= \() -> build e) (const $ return ()) (next:nexts)
+runRenderM (RenderM rm) = runReaderT rm
 
-    continue :: Responder -> IO ()
-    continue next = do
-      e <- liftIO $ nextEvent vty
-      runRenderM (next e) vty
-
-type Handler a = a -> RenderM ()
-
-fire :: Handler a -> a -> RenderM ()
-fire send a =
-  modify (\s -> (send a : fst s, snd s))
-
-handler :: Responder -> RenderM ()
-handler h =
-  modify (\s -> (fst s, \e -> snd s e >>= \() -> h e))
+type Handler a s = a -> SendResult s
 
 -- a picture and function that reacts to events
-type UI a = Handler a -> (Image, Responder)
+newtype UI a = UI (forall s. Handler a s -> (Image, Responder s))
 
 type Component' w m = w (UI (m ()))
 type Component w = Component' w (Co w)
@@ -81,7 +61,7 @@ componentMapAction :: forall m1 m2 w.
 componentMapAction f = fmap transformUi
   where
     transformUi :: UI (m1 ()) -> UI (m2 ())
-    transformUi ui1 send = ui1 (send <<< f)
+    transformUi (UI ui1) = UI $ \send -> ui1 (send <<< f)
 
 stateToCoStore :: forall f g a s. State s a -> Co (Store s) a
 stateToCoStore state = co (story state)
@@ -115,15 +95,15 @@ pairWriterTraced pairing f (WriterT writer) (TracedT gf) =
 explore :: forall w m. Comonad w => Pairing m w -> Component' w m -> RenderM ()
 explore pair space = do
     vty <- ask
-    let (img, runner) = extract space send
+    let (img, runner) = let (UI ui) = extract space in ui send
     let pic = picForImage img
     liftIO $ update vty pic
-    handler runner
+    e <- liftIO $ nextEvent vty
+    explore pair (appEndo (runner e) space)
     where
-      send :: m () -> RenderM ()
+      send :: m () -> SendResult (Component' w m)
       send action =
-        let space' = (pair (const id) action <<< duplicate) space in
-        explore pair space'
+         Endo (pair (const id) action <<< duplicate)
 
 exploreCo :: forall w. Comonad w => Component w -> RenderM ()
 exploreCo = explore (pairSym pairCo)
@@ -137,8 +117,9 @@ combine :: forall w1 w2
         -> Component (Day w1 w2)
 combine with w1 = day (build <$> w1) where
   build :: UI (Co w1 ()) -> UI (Co w2 ()) -> UI (Co (Day w1 w2) ())
-  build render1 render2 = with (\send -> render1 $ \co -> send (liftLeft co))
-                               (\send -> render2 $ \co -> send (liftRight co))
+  build (UI render1) (UI render2) =
+      with (UI (\send -> render1 $ \co -> send (liftLeft co)))
+           (UI (\send -> render2 $ \co -> send (liftRight co)))
 
 liftLeft :: forall w w' a. Functor w => Comonad w' => Co w a -> Co (Day w w') a
 liftLeft a = co (\(Day w w' f) -> runCo a (fmap (`f` extract w') w))
@@ -156,35 +137,37 @@ instance Monoid AddingInt where
 tracedExample :: Component' (Traced AddingInt) (Writer AddingInt)
 tracedExample = traced render where
   render :: AddingInt -> UI (Writer AddingInt ())
-  render (AddingInt count) send =
+  render (AddingInt count) = UI $ \send ->
     let line0 = text (defAttr ` withForeColor ` green) ("Traced " <> show count <> " line")
         line1 = string (defAttr ` withBackColor ` blue) "second line"
         img = line0 <|> line1
     in
-    (img, const $
-        when (count < 3) $
-          fire send (tell (AddingInt 1)))
+    (img, const $ if count < 3 then
+            send (tell (AddingInt 1))
+          else
+            Endo id)
 
 storeExample :: Component' (Store Int) (State Int)
 storeExample = store render 0 where
   render :: Int -> UI (State Int ())
-  render count send =
+  render count = UI $ \send ->
     let line0 = text (defAttr ` withForeColor ` green) ("Store " <> show count <> " line")
         line1 = string (defAttr ` withBackColor ` blue) "second line"
         img = line0 <|> line1
     in
     (img, const $
-        when (count < 3) $
-          fire send (modify (+1)))
+        if count < 3 then
+          send (modify (+1))
+        else Endo id)
 
 combinedExample :: Component (Day (Store Int) (Traced AddingInt))
 combinedExample = combine with store traced
   where
     with :: forall a. UI a -> UI a -> UI a
-    with ui1 ui2 send =
+    with (UI ui1) (UI ui2) = UI $ \send ->
       let (pic1, h1) = ui1 send in
       let (pic2, h2) = ui2 send in
-      (pic1 <|> pic2, \e -> h1 e >>= \() -> h2 e)
+      (pic1 <|> pic2, \e -> h1 e <> h2 e)
 
     traced :: Component (Traced AddingInt)
     traced = componentMapAction writerToCoTraced tracedExample
