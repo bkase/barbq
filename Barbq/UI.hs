57fb8a0d1691e14b9619316f17914662e9fd61b1
Brandon Kase <bkase@o1labs.org>
2019-11-17 00:37:53 -0500

    Use ormolu reformatter

--- Barbq/UI.hs
+++ Barbq/UI.hs
@@ -1,27 +1,35 @@
-{-# LANGUAGE KindSignatures, RankNTypes, NoImplicitPrelude, DeriveFunctor, GeneralizedNewtypeDeriving, ScopedTypeVariables, OverloadedStrings #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-module Barbq.UI (
-  pairStateStore,
-  pairWriterTraced,
-  pairIdentity,
-  explore,
-  storeExample,
-  tracedExample,
-  combinedExample,
-  exploreCo,
-  pairSym,
-  pairCo,
-  pairDay,
-  runRenderM) where
+module Barbq.UI
+  ( pairStateStore,
+    pairWriterTraced,
+    pairIdentity,
+    explore,
+    storeExample,
+    tracedExample,
+    combinedExample,
+    exploreCo,
+    pairSym,
+    pairCo,
+    pairDay,
+    runRenderM
+    )
+where
 
-import Relude hiding ((<|>))
-import Graphics.Vty
-import Control.Monad.Co
-import Control.Comonad (Comonad, extract, duplicate)
+import Control.Comonad (Comonad, duplicate, extract)
 import Control.Comonad.Store
-import Control.Monad.Writer (runWriter, Writer, tell, WriterT(..))
 import Control.Comonad.Traced
+import Control.Monad.Co
+import Control.Monad.Writer (Writer, WriterT (..), runWriter, tell)
 import Data.Functor.Day
+import Graphics.Vty
+import Relude hiding ((<|>))
 
 type Pairing f g = forall a b c. (a -> b -> c) -> f a -> g b -> c
 
@@ -33,11 +41,12 @@ pairSym pairing f ga fb = pairing (flip f) fb ga
 
 pairDay :: forall f1 f2 g1 g2. Pairing f1 f2 -> Pairing g1 g2 -> Pairing (Day f1 g1) (Day f2 g2)
 pairDay p1 p2 f (Day f1 g1 g) (Day f2 g2 h) =
-  let (x1, x2) = p1 (,) f1 f2 in
-  let (y1, y2) = p2 (,) g1 g2 in
-  f (g x1 y1) (h x2 y2)
+  let (x1, x2) = p1 (,) f1 f2
+   in let (y1, y2) = p2 (,) g1 g2
+       in f (g x1 y1) (h x2 y2)
 
 type SendResult s = Endo s
+
 type Responder s = Event -> SendResult s
 
 newtype RenderM a = RenderM (ReaderT Vty IO a)
@@ -52,12 +61,14 @@ type Handler a s = a -> SendResult s
 newtype UI a = UI (forall s. (Handler a s -> WriterT (Responder s) Identity Image))
 
 type Component' w m = w (UI (m ()))
+
 type Component w = Component' w (Co w)
 
-componentMapAction :: forall m1 m2 w.
-                      Functor w => (m1 () -> m2 ()) ->
-                      Component' w m1 ->
-                      Component' w m2
+componentMapAction
+  :: forall m1 m2 w. Functor w
+  => (m1 () -> m2 ())
+  -> Component' w m1
+  -> Component' w m2
 componentMapAction f = fmap transformUi
   where
     transformUi :: UI (m1 ()) -> UI (m2 ())
@@ -68,18 +79,18 @@ stateToCoStore state = co (story state)
   where
     story :: forall r. State s a -> Store s (a -> r) -> r
     story state store =
-      let (render, s) = runStore store in
-      let (a, s') = runState state s in
-      render s' a
+      let { (render, s) = runStore store }
+       in let { (a, s') = runState state s }
+           in render s' a
 
 writerToCoTraced :: forall f g a s. Monoid s => Writer s a -> Co (Traced s) a
 writerToCoTraced writer = co (tracer writer)
   where
     tracer :: forall r. Writer s a -> Traced s (a -> r) -> r
     tracer writer traced =
-      let (a, s) = runWriter writer in
-      let f = runTraced traced s in
-      f a
+      let { (a, s) = runWriter writer }
+       in let { f = runTraced traced s }
+           in f a
 
 pairIdentity :: Pairing Identity Identity
 pairIdentity f fa gb = runIdentity (liftA2 f fa gb)
@@ -94,71 +105,77 @@ pairWriterTraced pairing f (WriterT writer) (TracedT gf) =
 
 explore :: forall w m. Comonad w => Pairing m w -> Component' w m -> RenderM ()
 explore pair space = do
-    vty <- ask
-    let (img, runner) = let (UI ui) = extract space in runWriter (ui send)
-    let pic = picForImage img
-    liftIO $ update vty pic
-    e <- liftIO $ nextEvent vty
-    explore pair (appEndo (runner e) space)
-    where
-      send :: m () -> SendResult (Component' w m)
-      send action =
-         Endo (pair (const id) action <<< duplicate)
+  vty <- ask
+  let (img, runner) = let { (UI ui) = extract space } in runWriter (ui send)
+  let pic = picForImage img
+  liftIO $ update vty pic
+  e <- liftIO $ nextEvent vty
+  explore pair (appEndo (runner e) space)
+  where
+    send :: m () -> SendResult (Component' w m)
+    send action =
+      Endo (pair (const id) action <<< duplicate)
 
 exploreCo :: forall w. Comonad w => Component w -> RenderM ()
 exploreCo = explore (pairSym pairCo)
 
-combine :: forall w1 w2
-         . Comonad w1
-        => Comonad w2
-        => (forall a. UI a -> UI a -> UI a)
-        -> Component w1
-        -> Component w2
-        -> Component (Day w1 w2)
-combine with w1 = day (build <$> w1) where
-  build :: UI (Co w1 ()) -> UI (Co w2 ()) -> UI (Co (Day w1 w2) ())
-  build (UI render1) (UI render2) =
+combine
+  :: forall w1 w2. Comonad w1
+  => Comonad w2
+  => (forall a. UI a -> UI a -> UI a)
+  -> Component w1
+  -> Component w2
+  -> Component (Day w1 w2)
+combine with w1 = day (build <$> w1)
+  where
+    build :: UI (Co w1 ()) -> UI (Co w2 ()) -> UI (Co (Day w1 w2) ())
+    build (UI render1) (UI render2) =
       with (UI (\send -> render1 $ \co -> send (liftLeft co)))
-           (UI (\send -> render2 $ \co -> send (liftRight co)))
+        (UI (\send -> render2 $ \co -> send (liftRight co)))
 
-liftLeft :: forall w w' a. Functor w => Comonad w' => Co w a -> Co (Day w w') a
+liftLeft :: forall w w' a. Functor w => Comonad  w' => Co w a -> Co (Day w  w') a
 liftLeft a = co (\(Day w w' f) -> runCo a (fmap (`f` extract w') w))
 
-liftRight :: forall w w' a. Functor w => Comonad w' => Co w a -> Co (Day w' w) a
+liftRight :: forall w w' a. Functor w => Comonad  w' => Co w a -> Co (Day  w' w) a
 liftRight a = co (\(Day w' w f) -> runCo a (fmap (f (extract w')) w))
 
 newtype AddingInt = AddingInt Int
 
 instance Semigroup AddingInt where
   (<>) (AddingInt i1) (AddingInt i2) = AddingInt $ i1 + i2
+
 instance Monoid AddingInt where
   mempty = AddingInt 0
 
 tracedExample :: Component' (Traced AddingInt) (Writer AddingInt)
-tracedExample = traced render where
-  render :: AddingInt -> UI (Writer AddingInt ())
-  render (AddingInt count) = UI $ \send -> do
-    let line0 = text (defAttr ` withForeColor ` green) ("Traced " <> show count <> " line")
-        line1 = string (defAttr ` withBackColor ` blue) "second line"
-        img = line0 <|> line1
-    () <- tell $ const $ if count < 3 then
-            send (tell (AddingInt 1))
-          else
-            Endo id
-    return img
+tracedExample = traced render
+  where
+    render :: AddingInt -> UI (Writer AddingInt ())
+    render (AddingInt count) = UI $ \send -> do
+      let line0 = text (defAttr `withForeColor` green) ("Traced " <> show count <> " line")
+          line1 = string (defAttr `withBackColor` blue) "second line"
+          img = line0 <|> line1
+      () <-
+        tell $ const
+          $ if count < 3
+            then send (tell (AddingInt 1))
+            else Endo id
+      return img
 
 storeExample :: Component' (Store Int) (State Int)
-storeExample = store render 0 where
-  render :: Int -> UI (State Int ())
-  render count = UI $ \send -> do
-    let line0 = text (defAttr ` withForeColor ` green) ("Store " <> show count <> " line")
-        line1 = string (defAttr ` withBackColor ` blue) "second line"
-        img = line0 <|> line1
-    () <- tell $ const $ if count < 3 then
-            send (modify (+1))
-          else
-            Endo id
-    return img
+storeExample = store render 0
+  where
+    render :: Int -> UI (State Int ())
+    render count = UI $ \send -> do
+      let line0 = text (defAttr `withForeColor` green) ("Store " <> show count <> " line")
+          line1 = string (defAttr `withBackColor` blue) "second line"
+          img = line0 <|> line1
+      () <-
+        tell $ const
+          $ if count < 3
+            then send (modify (+ 1))
+            else Endo id
+      return img
 
 combinedExample :: Component (Day (Store Int) (Traced AddingInt))
 combinedExample = combine with store traced
@@ -168,12 +185,7 @@ combinedExample = combine with store traced
       pic1 <- ui1 send
       pic2 <- ui2 send
       return $ pic1 <|> pic2
-
     traced :: Component (Traced AddingInt)
     traced = componentMapAction writerToCoTraced tracedExample
-
     store :: Component (Store Int)
     store = componentMapAction stateToCoStore storeExample
-
-
-
--- Main.hs
+++ Main.hs
@@ -1,49 +1,66 @@
-{-# LANGUAGE LambdaCase, MultiParamTypeClasses, GeneralizedNewtypeDeriving, DeriveFunctor, NoImplicitPrelude, OverloadedStrings, DefaultSignatures, GADTs, KindSignatures, FlexibleInstances, TemplateHaskell, TypeFamilies, FlexibleContexts, ScopedTypeVariables, DerivingStrategies, DerivingVia, InstanceSigs, StandaloneDeriving, UndecidableInstances, RankNTypes #-}
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Main where
 
-import Relude hiding ((<|>))
 import Barbq.Types
 import Barbq.UI
-import System.Clock
-import Data.Semigroup ((<>))
-import Control.Monad.Co
-import Control.Comonad.Store
-import Control.Lens
-import Control.Concurrent (threadDelay)
-import UnliftIO.Concurrent (forkIO)
-import Control.Monad.Catch (MonadCatch, MonadMask, MonadThrow)
-import Control.Concurrent.Async (async)
-import Control.Concurrent.Async.Timer (withAsyncTimer, TimerConf, defaultConf, setInitDelay, setInterval, Timer, wait)
-import Graphics.Vty (standardIOConfig, mkVty, string, text, defAttr, withForeColor, green, picForImage, update, nextEvent, shutdown, blue, withBackColor, (<|>))
-import Control.Monad.IO.Unlift
 import Control.Applicative.Free as A
-import Pipes (Consumer, Producer, runEffect, (>->), yield, await)
-import Pipes.Concurrent (spawn, latest, toOutput, fromInput, unbounded, newest, Input, Output)
+import Control.Comonad.Store
+import Control.Concurrent (threadDelay)
+import Control.Concurrent.Async (async)
+import Control.Concurrent.Async.Timer (Timer, TimerConf, defaultConf, setInitDelay, setInterval, wait, withAsyncTimer)
+import Control.Lens
+import Control.Monad.Catch (MonadCatch, MonadMask, MonadThrow)
+import Control.Monad.Co
+import Control.Monad.IO.Unlift
+import Data.Semigroup ((<>))
+import Graphics.Vty ((<|>), blue, defAttr, green, mkVty, nextEvent, picForImage, shutdown, standardIOConfig, string, text, update, withBackColor, withForeColor)
+import Pipes ((>->), Consumer, Producer, await, runEffect, yield)
+import Pipes.Concurrent (Input, Output, fromInput, latest, newest, spawn, toOutput, unbounded)
 import qualified Pipes.Prelude as P
+import Relude hiding ((<|>))
+import System.Clock
+import UnliftIO.Concurrent (forkIO)
 
 --class Monad m => MonadChan chan m where
-  --newChan :: Int -> m (chan a)
-  ---- these block
-  --writeChan :: chan a -> a -> m ()
-  --readChan :: chan a -> m a
-
+--newChan :: Int -> m (chan a)
+---- these block
+--writeChan :: chan a -> a -> m ()
+--readChan :: chan a -> m a
 instance MonadIntervalRunner M where
   start :: forall o. TimerConf -> Output () -> M o -> o -> M (Input o)
   start conf triggerOutput task z = do
     (output, input) <- liftIO $ spawn (latest z)
     _tid <- forkIO $ withAsyncTimer conf (loop output)
     return input
-      where
-        loop output timer = do
-          runEffect $ runTask timer >-> toOutput (output <> contramap (const ()) triggerOutput)
-          loop output timer
-
-        runTask :: Timer -> Producer o M ()
-        runTask timer = do
-          lift $ liftIO $ wait timer
-          o <- lift task
-          yield o
+    where
+      loop output timer = do
+        runEffect $ runTask timer >-> toOutput (output <> contramap (const ()) triggerOutput)
+        loop output timer
+      runTask :: Timer -> Producer o M ()
+      runTask timer = do
+        lift $ liftIO $ wait timer
+        o <- lift task
+        yield o
 
 instance MonadShell M where
   execSh s =
@@ -58,9 +75,10 @@ runTask (ShellTask s f) = do
   f <$> execSh s
 
 newtype ProviderRuntime m a = ProviderRuntime (m (Input a))
-  deriving Functor
+  deriving (Functor)
 
 instance Applicative (ProviderRuntime M) where
+
   pure a = ProviderRuntime $ do
     (output, input) <- liftIO $ spawn (latest a)
     return input
@@ -82,16 +100,17 @@ provide conf task z = Provider $ liftAp (ProviderAtom conf task z)
 runProvider :: (MonadIntervalRunner m, MonadUnliftIO m, MonadShell m, Applicative (ProviderRuntime m)) => forall o. Output () -> Provider o -> m (Input o)
 runProvider triggerOutput (Provider freeAp) = minput
   where
-    (ProviderRuntime minput) = runAp (\atom -> ProviderRuntime $ do
-      let task' = runTask $ task atom
-      start (conf atom) triggerOutput task' (z atom)) freeAp
-
+    (ProviderRuntime minput) =
+      runAp
+        ( \atom -> ProviderRuntime $ do
+            let task' = runTask $ task atom
+            start (conf atom) triggerOutput task' (z atom)
+          )
+        freeAp
     conf :: ProviderAtom a -> TimerConf
     conf = view providerConf
-
     z :: ProviderAtom a -> a
     z = view providerDefault
-
     task :: ProviderAtom a -> Task a
     task = view providerTask
 
@@ -102,22 +121,18 @@ runProvider triggerOutput (Provider freeAp) = minput
 
 --counter :: Component (Store Int)
 --counter = store render 0
-  --where
-    --render :: Int -> UI (Co (Store Int) ())
-    --render count send =
-      --send "hello"
-
+--where
+--render :: Int -> UI (Co (Store Int) ())
+--render count send =
+--send "hello"
 app :: M ()
 app = do
   -- we send unit to unblock so we can use latest
   (outputU, inputU) <- liftIO $ spawn (newest 1)
-
   -- (purely) describe the providers
-  let tupled = (\a b c d -> (a,b,c,d)) <$> provide (after 1000 & everyi 2000) (shell "kwm...") "?" <*> provide (after 300 & everyi 1500) (shell "foo..") "?" <*> provide (after 200 & everyi 4000) (shell "bar..") "?" <*> provide (after 20000 & everyi 40000) (shell "bar..") "?"
-
+  let tupled = (\a b c d -> (a, b, c, d)) <$> provide (after 1000 & everyi 2000) (shell "kwm...") "?" <*> provide (after 300 & everyi 1500) (shell "foo..") "?" <*> provide (after 200 & everyi 4000) (shell "bar..") "?" <*> provide (after 20000 & everyi 40000) (shell "bar..") "?"
   -- run the provider
   inputG <- runProvider outputU tupled
-
   -- dump provided data to stdout
   liftIO $ runEffect $ fromInput inputG >-> handler inputU
   where
@@ -138,4 +153,3 @@ main = do
   shutdown vty
   print ("Last event was: " ++ show 3)
   runApp app
-
